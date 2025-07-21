faa2cde09a2151b0adececaed3c2565a700031e3
DARKWOLF <jabulanichizimba1@gmail.com>
2025-07-21 17:20:43 +0200

    making a card game

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum suits {Heart, Spade, Diamond, Clover};
+
+
+class Cards
+{
+private:
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
